026f73ca4c809cd3cee6c11c3529baf889fa6bef
Lavezzi7891 <luca_damires@hotmail.com>
2016-08-12 15:13:17 +0200

    Het werkt...

---
+++ spelers_tervant.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>A.C. Tervant U10</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<div class="main">
+	<h1>A.C. Tervant U10A</h1>
+	<div class="header">
+		<div class="menu">
+			<ul>
+			<li>Spelers</li>
+			<li>Spelers statistieken</li>
+			<li>Ploeg doelpunten</li>
+		</ul>
+		</div>
+	</div>
+	<div class="inner-body"></div>
+</div>
+
+ 
+</body>
+</html>
